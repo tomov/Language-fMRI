20534a00fffc534d6e743fb4ef00ab564a559c2b
Momchil Tomov <tomov90@gmail.com>
2019-08-18 20:58:22 -0400

    save_vol

---
+++ save_vol.m
@@ -0,0 +1,40 @@
+function info = save_vol(vol, filename)
+
+    % usage: save_vol(vollangloc, 'temp/test.nii');
+    %        bspmview('temp/test.nii');
+
+    [~, V, Y] = ccnl_load_mask('mask_from_uncertainty_study.nii');
+    Y(:) = 0;
+    Y(1:size(vol,1), 1:size(vol,2), 1:size(vol,3)) = vol;
+    V.fname = filename; % !!!!!!!!!!!!!!! important!!!!!!
+    spm_write_vol(V, Y);
+
+    info = niftiinfo(filename);
+    %{
+    niftiwrite(V, filename);
+    info = niftiinfo(filename);
+    info.PixelDimensions = [2 2 2];
+    info.MultiplicativeScaling = 1;
+    info.SpatialDimension = 0;
+    info.SpaceUnits = 'Millimeter';
+    info.Transform = affine3d([-2 0 0 0; 0 2 0 0; 0 0 2 0; 78 -112 -60 1]);
+    info.TransformName = 'Sform';
+    info.Qfactor = -1;
+
+    info.raw.pixdim = [-1 2 2 2 0 0 0 0];
+    info.raw.scl_slope = -1;
+    info.raw.xyzt_units = 10;
+    info.raw.qform_code = 2;
+    info.raw.sform_code = 2;
+    info.raw.quatern_c = 1;
+    info.raw.qoffset_x = 78;
+    info.raw.qoffset_y = -112;
+    info.raw.qoffset_z = -60;
+    info.raw.srow_x = [-2 0 0 78];
+    info.raw.srow_y = [0 2 0 -112];
+    info.raw.srow_z = [0 0 2 -60];
+    info.raw.scl_slope = 1;
+    info.raw.descrip = 'spm_spm:resultant analysis mask';
+
+    niftiwrite(V, filename, info);
+    %}
---
+++ scp_to_ncf.sh
@@ -0,0 +1,20 @@
+#scp mtomov13@ncflogin.rc.fas.harvard.edu:/ncf/gershman/Lab/scripts/matlab/Exploration/cross_subj_perf_bic_roiglm29_DV_glm29_sphere_standardize=0_corr=0_extent=100.mat .
+#scp mtomov13@ncflogin.rc.fas.harvard.edu:/ncf/gershman/Lab/scripts/matlab/Exploration/univariate_decoder_both_badre_glm45_RUroi=1_TUroi=2_orth=0_lambda=1.000000_standardize=2_mixed=1_corr=0_extent=100_Num=1_intercept=1_flip=1.mat .
+#scp mtomov13@ncflogin.rc.fas.harvard.edu:/ncf/gershman/Lab/scripts/matlab/Exploration/univariate_decoder_refactored_roiglm29_DV_glm29_DV_orth=0_lambda=1.000000_standardize=2_mixed=1_corr=0_extent=100_Num=1_intercept=1_flip=1_doCV=0_gn=0_s=10.0.mat . 
+
+
+scp mtomov13@ncflogin.rc.fas.harvard.edu:/ncf/gershman/Lab/scripts/matlab/Language-fMRI/region_searchlight5.mat .
+#scp mtomov13@ncflogin.rc.fas.harvard.edu:/ncf/gershman/Lab/scripts/matlab/Exploration/\{jobs.txt,goodjobs.txt\} .
+#scp mtomov13@ncflogin.rc.fas.harvard.edu:/ncf/gershman/Lab/scripts/matlab/Exploration/output.tar.gz .
+#scp mtomov13@ncflogin.rc.fas.harvard.edu:/ncf/gershman/Lab/scripts/matlab/Exploration/run_ccnl_fmri_glm_parallel_jobs.sh .
+
+#scp mtomov13@ncflogin.rc.fas.harvard.edu:/ncf/gershman/Lab/scripts/matlab/Exploration/\{univariate_decoder_refactored_roiglm-1_badre_glm45_RU_orth=0_lambda=1.000000_standardize=2_mixed=1_corr=0_extent=100_Num=1_intercept=1_flip=1_doCV=0_gn=0_s=10.0.mat,univariate_decoder_refactored_roiglm-1_badre_glm45_TU_orth=0_lambda=1.000000_standardize=2_mixed=1_corr=0_extent=100_Num=1_intercept=1_flip=1_doCV=0_gn=0_s=10.0.mat,univariate_decoder_refactored_roiglm-1_dlpfc_glm45_RU_orth=0_lambda=1.000000_standardize=2_mixed=1_corr=0_extent=100_Num=1_intercept=1_flip=1_doCV=0_gn=0_s=10.0.mat,univariate_decoder_refactored_roiglm-1_dlpfc_glm45_TU_orth=0_lambda=1.000000_standardize=2_mixed=1_corr=0_extent=100_Num=1_intercept=1_flip=1_doCV=0_gn=0_s=10.0.mat\} .
+
+#scp *.m  mtomov13@ncflogin.rc.fas.harvard.edu:/ncf/gershman/Lab/scripts/matlab/Exploration
+#scp   mtomov13@ncflogin.rc.fas.harvard.edu:/ncf/gershman/Lab/scripts/matlab/Exploration/output.tar.gz .
+#scp  ../../libs/mfit/hfit*  mtomov13@ncflogin.rc.fas.harvard.edu:/ncf/gershman/Lab/scripts/matlab/Exploration/
+#scp  *OpAL*.sh *AU*.sh *ACU*.sh *OpAL*.m *AU*.m *ACU*.m  mtomov13@ncflogin.rc.fas.harvard.edu:/ncf/gershman/Lab/scripts/matlab/Exploration/
+#scp  AU.m fit_AU.m ACU.m fit_ACU.m OpAL.m fit_OpAL.m  mtomov13@ncflogin.rc.fas.harvard.edu:/ncf/gershman/Lab/scripts/matlab/Exploration/
+#scp  fit_AU.sh  fit_ACU.sh  fit_OpAL.sh  mtomov13@ncflogin.rc.fas.harvard.edu:/ncf/gershman/Lab/scripts/matlab/Exploration/
+#scp    mtomov13@ncflogin.rc.fas.harvard.edu:/ncf/gershman/Lab/scripts/matlab/Exploration/exploration_getSu* .
+#scp mtomov13@ncflogin.rc.fas.harvard.edu:/ncf/tools/current/code/bin/ArcGet.py .
